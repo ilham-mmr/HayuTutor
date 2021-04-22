c62a5232f08398728d703b96e809c51eb329d763
Ilham MMR <ilham.mmr@gmail.com>
2021-04-23 04:47:16 +0700

    delete unused import

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutapp/models/user.dart';
-import 'package:flutapp/screens/newpassword_screen.dart';
 import 'package:flutapp/screens/splash_screen.dart';
 import 'package:flutapp/utils/user_preferences.dart';
 
@@ -23,15 +22,6 @@ class MyApp extends StatelessWidget {
       child: MaterialApp(
         title: 'HayuTutor',
         theme: ThemeData(
-            // This is the theme of your application.
-            //
-            // Try running your application with "flutter run". You'll see the
-            // application has a blue toolbar. Then, without quitting the app, try
-            // changing the primarySwatch below to Colors.green and then invoke
-            // "hot reload" (press "r" in the console where you ran "flutter run",
-            // or simply save your changes to "hot reload" in a Flutter IDE).
-            // Notice that the counter didn't reset back to zero; the application
-            // is not restarted.
             primaryColor: Color(0xFF1D4E56),
             accentColor: Colors.cyan[800],
             textSelectionTheme: TextSelectionThemeData(
