3dc8b2860d7a22bf3d596c6c977bf1ee9e1714a3
Ilham MMR <ilham.mmr@gmail.com>
2021-04-23 04:49:00 +0700

    move the content container column into stack and add icon arrow back button into the stack

--- lib/screens/signin_screen.dart
+++ lib/screens/signin_screen.dart
@@ -1,8 +1,8 @@
+import 'package:flutapp/helpers/noanimation_page_route.dart';
 import 'package:flutapp/models/user.dart';
 import 'package:flutapp/screens/forgot_validation_screen.dart';
-import 'package:flutapp/screens/newpassword_screen.dart';
 import 'package:flutapp/screens/signup_screen.dart';
-import 'package:flutapp/screens/welcome.dart';
+import 'package:flutapp/screens/home_screen.dart';
 import 'package:flutapp/utils/user_preferences.dart';
 import 'package:flutapp/mixins/validator.dart';
 import 'package:flutter/cupertino.dart';
@@ -28,170 +28,197 @@ class _SigninScreenState extends State<SigninScreen> with Validator {
 
   @override
   Widget build(BuildContext context) {
+    double height = MediaQuery.of(context).size.height;
+
     return Scaffold(
       body: SafeArea(
         child: SingleChildScrollView(
           child: Container(
-            child: Container(
-              margin: EdgeInsets.all(20),
-              padding: EdgeInsets.all(18),
-              child: Column(
-                children: [
-                  Container(
-                    height: 200,
-                    child: Image.asset(
-                      'images/logo.png',
-                    ),
-                  ),
-                  Container(
-                    margin: EdgeInsets.symmetric(vertical: 18),
-                    child: Row(
-                      children: [
-                        Text(
-                          'Login',
-                          textAlign: TextAlign.left,
-                          style: TextStyle(
-                              fontSize: 30, fontWeight: FontWeight.bold),
-                        ),
-                      ],
-                    ),
-                  ),
-                  SizedBox(
-                    height: 12,
-                  ),
-                  Form(
-                    key: _formKey,
-                    child: Column(
-                      children: [
-                        TextFormField(
-                          initialValue: _email,
-                          onChanged: (value) {
-                            _email = value;
-                          },
-                          keyboardType: TextInputType.emailAddress,
-                          decoration: InputDecoration(
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(10),
+            margin: EdgeInsets.all(6),
+            child: Stack(
+              children: [
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: IconButton(
+                      icon: Icon(Icons.arrow_back),
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      }),
+                ),
+                Container(
+                  margin: EdgeInsets.all(20),
+                  padding: EdgeInsets.all(18),
+                  child: Column(
+                    children: [
+                      Column(
+                        children: [
+                          Container(
+                            height: height * 0.28,
+                            child: Image.asset(
+                              'images/logo.png',
                             ),
-                            labelText: 'Email Address',
-                            labelStyle: TextStyle(
-                              fontSize: 20,
-                            ),
-                            hintText: 'youremail@example.com',
-                            prefixIcon: Icon(Icons.email),
                           ),
-                          validator: validateEmail,
-                          onSaved: (newValue) => _email = newValue,
-                        ),
-                        SizedBox(
-                          height: 18,
-                        ),
-                        TextFormField(
-                          obscureText: true,
-                          decoration: InputDecoration(
-                              border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(10),
-                              ),
-                              prefixIcon: Icon(Icons.lock),
-                              labelText: 'Password',
-                              hintText: 'Password',
-                              labelStyle: TextStyle(
-                                fontSize: 20,
-                              ),
-                              suffixIcon: Padding(
-                                  padding: EdgeInsets.all(15),
-                                  child: GestureDetector(
-                                    onTap: _forgotPassword,
-                                    child: Text(
-                                      'forgot?',
-                                      style: _boldCyanTextStyle(context),
+                          Container(
+                            margin: EdgeInsets.symmetric(vertical: 18),
+                            child: Row(
+                              children: [
+                                Text(
+                                  'Login',
+                                  textAlign: TextAlign.left,
+                                  style: TextStyle(
+                                      fontSize: 30,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                              ],
+                            ),
+                          ),
+                          SizedBox(
+                            height: 12,
+                          ),
+                          Form(
+                            key: _formKey,
+                            child: Column(
+                              children: [
+                                TextFormField(
+                                  initialValue: _email,
+                                  onChanged: (value) {
+                                    _email = value;
+                                  },
+                                  keyboardType: TextInputType.emailAddress,
+                                  decoration: InputDecoration(
+                                    border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(10),
                                     ),
-                                  ))),
-                          validator: validatePassword,
-                          onSaved: (newValue) => _password = newValue,
-                        ),
-                        SizedBox(
-                          height: 18,
-                        ),
-                        Row(
-                          children: [
-                            Text('New to this app?'),
-                            GestureDetector(
-                              onTap: () async {
-                                await UserPreferences.setEmail(_email);
-                                Navigator.pushReplacement(
-                                  context,
-                                  MaterialPageRoute(
-                                      builder: (context) => SignupScreen()),
-                                );
-                              },
-                              child: Text(
-                                ' Sign Up',
-                                style: _boldCyanTextStyle(context),
-                              ),
-                            )
-                          ],
-                        ),
-                        SizedBox(
-                          height: 18,
-                        ),
-                        //check if it's loading
-                        _isLoading
-                            ? Center(
-                                child: CircularProgressIndicator(),
-                              )
-                            : CircleAvatar(
-                                backgroundColor: Theme.of(context).primaryColor,
-                                child: IconButton(
-                                  icon: Icon(Icons.arrow_forward),
-                                  onPressed: () async {
-                                    // Validate returns true if the form is valid, or false otherwise.
-                                    if (_formKey.currentState.validate()) {
-                                      // If the form is valid, display a snackbar. In the real world,
-                                      // you'd often call a server or save the information in a database.
-                                      _formKey.currentState
-                                          .save(); // call onSaved
-
-                                      setState(() {
-                                        _isLoading = true;
-                                      });
-                                      bool isSignedIn = await Provider.of<User>(
-                                              context,
-                                              listen: false)
-                                          .signIn(_email, _password);
-
-                                      // bool isSignedIn =
-                                      //     await user.signIn(_email, _password);
-
-                                      setState(() {
-                                        _isLoading = false;
-                                      });
-
-                                      if (isSignedIn) {
+                                    labelText: 'Email Address',
+                                    labelStyle: TextStyle(
+                                      fontSize: 20,
+                                    ),
+                                    hintText: 'youremail@example.com',
+                                    prefixIcon: Icon(Icons.email),
+                                  ),
+                                  validator: validateEmail,
+                                  onSaved: (newValue) => _email = newValue,
+                                ),
+                                SizedBox(
+                                  height: 18,
+                                ),
+                                TextFormField(
+                                  obscureText: true,
+                                  decoration: InputDecoration(
+                                      border: OutlineInputBorder(
+                                        borderRadius: BorderRadius.circular(10),
+                                      ),
+                                      prefixIcon: Icon(Icons.lock),
+                                      labelText: 'Password',
+                                      hintText: 'Password',
+                                      labelStyle: TextStyle(
+                                        fontSize: 20,
+                                      ),
+                                      suffixIcon: Padding(
+                                          padding: EdgeInsets.all(15),
+                                          child: GestureDetector(
+                                            onTap: _forgotPassword,
+                                            child: Text(
+                                              'forgot?',
+                                              style:
+                                                  _boldCyanTextStyle(context),
+                                            ),
+                                          ))),
+                                  validator: validatePassword,
+                                  onSaved: (newValue) => _password = newValue,
+                                ),
+                                SizedBox(
+                                  height: 18,
+                                ),
+                                Row(
+                                  children: [
+                                    Text('New to this app?'),
+                                    GestureDetector(
+                                      onTap: () async {
+                                        await UserPreferences.setEmail(_email);
                                         Navigator.pushReplacement(
                                           context,
-                                          MaterialPageRoute(
-                                            builder: (context) => Welcome(),
-                                          ),
+                                          NoAnimationMaterialPageRoute(
+                                              builder: (context) =>
+                                                  SignupScreen()),
                                         );
-                                      } else {
-                                        ScaffoldMessenger.of(context)
-                                            .showSnackBar(
-                                          SnackBar(
-                                            content:
-                                                Text('invalid email/password'),
-                                          ),
-                                        );
-                                      }
-                                    }
-                                  },
+                                      },
+                                      child: Text(
+                                        ' Sign Up',
+                                        style: _boldCyanTextStyle(context),
+                                      ),
+                                    )
+                                  ],
                                 ),
-                              ),
-                      ],
-                    ),
-                  )
-                ],
-              ),
+                                SizedBox(
+                                  height: 18,
+                                ),
+                                //check if it's loading
+                                _isLoading
+                                    ? Center(
+                                        child: CircularProgressIndicator(),
+                                      )
+                                    : CircleAvatar(
+                                        backgroundColor:
+                                            Theme.of(context).primaryColor,
+                                        child: IconButton(
+                                          icon: Icon(Icons.arrow_forward),
+                                          onPressed: () async {
+                                            // Validate returns true if the form is valid, or false otherwise.
+                                            if (_formKey.currentState
+                                                .validate()) {
+                                              // If the form is valid, display a snackbar. In the real world,
+                                              // you'd often call a server or save the information in a database.
+                                              _formKey.currentState
+                                                  .save(); // call onSaved
+
+                                              setState(() {
+                                                _isLoading = true;
+                                              });
+                                              bool isSignedIn =
+                                                  await Provider.of<User>(
+                                                          context,
+                                                          listen: false)
+                                                      .signIn(
+                                                          _email, _password);
+
+                                              // bool isSignedIn =
+                                              //     await user.signIn(_email, _password);
+
+                                              setState(() {
+                                                _isLoading = false;
+                                              });
+
+                                              if (isSignedIn) {
+                                                Navigator.pushReplacement(
+                                                  context,
+                                                  NoAnimationMaterialPageRoute(
+                                                    builder: (context) =>
+                                                        HomeScreen(),
+                                                  ),
+                                                );
+                                              } else {
+                                                ScaffoldMessenger.of(context)
+                                                    .showSnackBar(
+                                                  SnackBar(
+                                                    content: Text(
+                                                        'invalid email/password'),
+                                                  ),
+                                                );
+                                              }
+                                            }
+                                          },
+                                        ),
+                                      ),
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
           ),
         ),
