692333ed4d6e3d3e1210a4733b81934fdf9ea479
Ilham MMR <ilham.mmr@gmail.com>
2021-04-29 02:04:39 +0700

    add new screens

---
+++ lib/screens/explore_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutapp/widgets/bottom_nav_FAB.dart';
+import 'package:flutter/material.dart';
+
+class ExploreScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).accentColor,
+        // leading: Container(),
+        elevation: 0,
+        actions: [
+          IconButton(icon: Icon(Icons.favorite), onPressed: () {}),
+        ],
+      ),
+      body: Container(
+        child: Center(
+          child: Card(
+            child: Text('explore'),
+          ),
+        ),
+      ),
+    );
+  }
+}
